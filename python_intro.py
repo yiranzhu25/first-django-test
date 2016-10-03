4e7df2850149ee0c89a80c9cb021505b340f47be
yiranzhu25 <candytiimer@gmail.com>
2016-10-03 14:07:19 -0400

    create my first django test

---
+++ python_intro.py
@@ -0,0 +1,23 @@
+#volume = 33
+#if volume < 20:
+#    print("first answer")
+#elif 20<=volume<40:
+#    print("second answer")
+#elif 40<=volume<50:
+#    print('third answer')
+#else:
+#    print("last answer")
+
+#def hi():
+#    print("hi there")
+#    print("how are you")
+
+#hi()
+
+#def hi(name):
+#    print('Hi' + name + '!')
+
+#girls = ['Rachel', 'Monica', 'Phoebe']
+#for name in girls:
+#    hi(name)
+#    print('Next girl')
